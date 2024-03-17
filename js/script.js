d52f179dbe7e05979f6ab88e8400fca5697fdb20
Tiago Silva <tiagobsb84@gmail.com>
2024-03-16 21:01:47 -0300

    adicionando uma imagem no home e efeito na lista de navegacao

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-const titulo = document.querySelector('.digitando');
+//const titulo = document.querySelector('.digitando');
+const titulo = document.querySelector('.aparece_skill');
 
-function ativaLetra(elemento) {
+const array_skill = ["Java", "Javascript", "PHP", "ReactJS"];
+var contador_array = 0;
+var elemento;
+
+function proximaPalavra() {
+    contador_array++;
+    titulo.style.opacity = 0;
+    if(contador_array > array_skill.length -1) {
+        contador_array = 0;
+    }
+    setTimeout('slideArray()', 700);
+}
+
+function slideArray() {
+    titulo.innerHTML = array_skill[contador_array];
+    titulo.style.opacity = 1;
+    setTimeout('proximaPalavra()', 2000);
+}
+
+slideArray();
+/*function ativaLetra(elemento) {
     const arrayTexto = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     arrayTexto.forEach((letra, i) => {
@@ -8,6 +29,6 @@ function ativaLetra(elemento) {
             elemento.innerHTML += letra;
         }, 75 * i)
     });
-}
+}*/
 
-ativaLetra(titulo);
+//ativaLetra(titulo);
